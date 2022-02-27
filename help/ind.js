d7430d67d23e02307f2ad4a2f5dc03e62301d9e5
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-02-27 19:52:18 +0800

    Update ind.js

--- help/ind.js
+++ help/ind.js
@@ -80,7 +80,7 @@ exports.listMenu = (time, salam, pushname, prefix) => {
     return `
 *Selamat ${salam} ${pushname} 😖*
 *⌚Time Server : ${time}*
-*📚 List-Menu Alphabot Beta :*
+*📚 List-Menu AZbot Beta :*
 
 🤖 *INFO*
 ├ ${prefix}owner
@@ -173,6 +173,7 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 ├ Xfarr api
 ├ Fatih A.
 ├ ZeeoneOfc
+├Aiman
 ├ Baileys#multi-device
 ├ And all support
 └──────
@@ -211,9 +212,9 @@ Sanksi: *‼️ PERMANENT BLOCK ‼️*
 
 Jika sudah dipahami rules-nya, silakan ketik *${prefix}allmenu* untuk memulai!
 
-⚠️ Segala kebijakan dan ketentuan Alphabot di pegang oleh owner dan segala perubahan kebijakan, sewaktu waktu owner berhak mencabut, memblokir user(*﹏*) 
+⚠️ Segala kebijakan dan ketentuan AZbot di pegang oleh owner dan segala perubahan kebijakan, sewaktu waktu owner berhak mencabut, memblokir user(*﹏*) 
 
-Arigatou Gozaimasu! Untuk kalian user ramah dan Beberapa orang yg ikut membantu juga dalam project pembuatan Alphabot
+Arigatou Gozaimasu! Untuk kalian user ramah dan Beberapa orang yg ikut membantu juga dalam project pembuatan AZbot
 😖🙏
 
 
@@ -223,7 +224,7 @@ exports.source = (prefix) =>{
 return`*── 「 SOURCE 」 ──*
 
 Original : https://github.com/rashidsiregar28/chikabot
-Sc Recode : https://github.com/zeeoneofc/Alphabot-Md
+Sc Recode : https://github.com/ai-man-123/ytmd
 `
 }
 exports.tos = (ownerNumber, prefix) => {
@@ -232,7 +233,7 @@ exports.tos = (ownerNumber, prefix) => {
 
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-🏧 0887435047326 (OVO/Dana/GoPay)
+🏧 0102810046 (money/Dana/GoPay)
 
 Berapapun donasi kalian akan sangat berarti 👍
 
